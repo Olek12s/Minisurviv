f7732284233a7a916f3dbbea2a17238e013346ec
Oleki <olekignaczak78@gmail.com>
2025-12-19 15:28:30 +0100

    first commit

---
+++ core/src/main/java/io/core/Main.java
@@ -0,0 +1,12 @@
+package io.core;
+
+import com.badlogic.gdx.Game;
+
+public class Main extends Game
+{
+
+    @Override
+    public void create() {
+        System.out.println("abc");
+    }
+}
---
+++ lwjgl3/src/main/java/io/core/lwjgl3/Packer.java
@@ -0,0 +1,12 @@
+package io.core.lwjgl3;
+
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+
+public class Packer
+{
+    public static void main(String[] args) {
+       // TexturePacker.process("assets/tiles", "assets", "tiles");
+      //  TexturePacker.process("assets/entities", "assets", "entities");
+      //  TexturePacker.process("assets/gui", "assets", "gui");
+    }
+}
