8df76ee53cc444f225dbef1d8436055efc88c14d
Oleki <olekignaczak78@gmail.com>
2025-12-27 23:58:30 +0100

    tweaking Rocky Biome

--- core/src/main/java/io/core/level/biome/biomes/RockyBiome.java
+++ core/src/main/java/io/core/level/biome/biomes/RockyBiome.java
@@ -2,15 +2,24 @@ package io.core.level.biome.biomes;
 
 import io.core.level.MapManager;
 import io.core.level.biome.Biome;
+import io.core.util.Noise;
 
 public class RockyBiome extends Biome
 {
     public RockyBiome() {
-        super(0.0f, 0.4f, 0.0f, 0.1f);
+        super(0, 9.4f, 0, 1.f);
     }
 
     @Override
     public void generate(MapManager map, int x, int y) {
-
     }
+
+    @Override
+    public float getGenerationWeight(Noise noise, int x, int y) {
+
+        return 2f;
+    }
+
+
+
 }
