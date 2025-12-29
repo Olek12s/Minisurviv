c449a9cd0a09288b63b7716135cbd89e77a7b006
Oleki <olekignaczak78@gmail.com>
2025-12-29 01:31:42 +0100

    Tiling system

--- core/src/main/java/io/core/level/tile/Tiles.java
+++ core/src/main/java/io/core/level/tile/Tiles.java
@@ -8,25 +8,24 @@ This one is tricky. Tile placing system has few rules:
 *****   BEDROCK - NON_SOLID - NON_SOLID
 *****   BEDROCK - NON_SOLID - SOLID
 3) Feature layer can be placed on bedrock or non-solid layer
+4) Liquids such as water or lava cannot be placed above solid ground layers
+5) Liquids cannot be stacked
  */
 public class Tiles
 {
     // FOUNDATION LAYER - THERE'S NOTHING DEEPER THAN BEDROCK
     public static Tile BEDROCK = new Tile("BEDROCK");
 
-    // GROUND LAYER #1 - connects only with foundation layer
+    // GROUND LAYER - there are 2 such layers in 1 tile
     public static Tile DIRT = new Tile("DIRT");
     public static Tile GRASS = new Tile("GRASS");
     public static Tile SAND = new Tile("SAND");
     public static Tile CLAY = new Tile("CLAY");
-
-    // GROUND LAYER #2 - connets to Ground layer #1 and foundation layer
     public static Tile WATER = new Tile("WATER");
     public static Tile STONE = new Tile("STONE");
 
 
     // FEATURE TILES - can connect to all other layers
-
     public static Tile DANTELION = new Tile("DANTELION");
     public static Tile ROSE = new Tile("ROSE");
     public static Tile SNOW = new Tile("SNOW");
