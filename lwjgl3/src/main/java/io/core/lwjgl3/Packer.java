2ad525aa953a6f50d91ae9f11db9540ac8d7c7e5
Oleki <olekignaczak78@gmail.com>
2026-01-01 18:40:11 +0100

    txt packer introduced

--- lwjgl3/src/main/java/io/core/lwjgl3/Packer.java
+++ lwjgl3/src/main/java/io/core/lwjgl3/Packer.java
@@ -5,8 +5,25 @@ import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 public class Packer
 {
     public static void main(String[] args) {
-       // TexturePacker.process("assets/tileDats", "assets", "tileDats");
-      //  TexturePacker.process("assets/entities", "assets", "entities");
-      //  TexturePacker.process("assets/gui", "assets", "gui");
+        packTextures();
+    }
+
+    /**
+     * TexturePacker provides an efficient algorithm for packing textures into a single file called an atlas.
+     * Packed textures can be found inside a given atlas by their names, such as "grass"
+     * (the same name as the packed file).
+     *
+     * Example:
+     * TexturePacker.process("assets/textures/tiles", "assets", "tiles");
+     * where "tiles" is the name of the atlas. This atlas contains all textures from the
+     * "assets/textures/tiles" folder.
+     *
+     * Finding a region inside a texture atlas is done as follows:
+     * TextureRegion grass = atlas.findRegion("grass");
+     *
+     * Drawing is done by passing a reference to the texture region.
+     */
+    private static void packTextures() {
+        TexturePacker.process("assets/textures/tiles", "assets", "tiles");
     }
 }
