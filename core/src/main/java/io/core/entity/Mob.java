b8fc1484afafe99a76d94897b3c32eef0d9a464b
Oleki <olekignaczak78@gmail.com>
2026-01-03 03:37:03 +0100

    basic player movement

--- core/src/main/java/io/core/entity/Mob.java
+++ core/src/main/java/io/core/entity/Mob.java
@@ -1,6 +1,11 @@
 package io.core.entity;
 
-public abstract class Mob
+import io.core.level.Level;
+
+public abstract class Mob extends Entity
 {
 
+    public void tick(Level level) {
+
+    }
 }
--- core/src/main/java/io/core/entity/Player.java
+++ core/src/main/java/io/core/entity/Player.java
@@ -1,6 +1,21 @@
 package io.core.entity;
 
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.ObjectFloatMap;
+import io.core.core.Input;
+import io.core.level.Level;
+
+
+
 public class Player extends Mob
 {
+    @Override
+    public void tick(Level level) {
+        Vector2 vec = new Vector2(0, 0);    // movement vector
 
+        if (Input.isHeld(Input.Keys.W)) vec.y++;    // up
+        if (Input.isHeld(Input.Keys.S)) vec.y--;    // down
+        if (Input.isHeld(Input.Keys.A)) vec.x--;    // left
+        if (Input.isHeld(Input.Keys.D)) vec.x++;    // right
+    }
 }
